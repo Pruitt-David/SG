eb774fd06c310d7690f3040ba94a114ca8fd2a5d
David Benjamin Pruitt <davidbpruitt2004@gmail.com>
2018-10-22 14:48:42 -0500

    hopefully allows view for content
    I think having it in a folder broke it so its all out a folder now

---
+++ countdown.js
@@ -0,0 +1,51 @@
+var deadline = new Date("Oct 30, 2018 12:34:56").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("CDDays").innerHTML = days;
+    
+    document.getElementById("CDHours").innerHTML = hours;   document.getElementById("CDMinutes").innerHTML = minutes;
+    document.getElementById("CDSeconds").innerHTML = seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("CD").innerHTML = "Its Out Now";
+    }
+}, 1000);
+
+
+var videoPlayButton,
+	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+        renderVideoPlayButton: function() {
+            if (videoWrapper.contains(video)) {
+				this.formatVideoPlayButton()
+                video.classList.add('has-media-controls-hidden')
+                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
+                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
+            }
+        },
+
+        formatVideoPlayButton: function() {
+            videoWrapper.insertAdjacentHTML('beforeend', '\
+                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
+                    <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
+                    <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
+                </svg>\
+            ')
+        },
+
+        hideVideoPlayButton: function() {
+            video.play()
+            videoPlayButton.classList.add('is-hidden')
+            video.classList.remove('has-media-controls-hidden')
+            video.setAttribute('controls', 'controls')
+        }
+	}
+
+videoMethods.renderVideoPlayButton()
+
